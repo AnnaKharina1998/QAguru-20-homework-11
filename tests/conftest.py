f20d3781743fab73f78457283cc990188fc15302
Анна Краснокутская <anna.kharina.1998@mail.ru>
2025-06-23 17:42:26 +0300

    hw 11.3

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "127.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
