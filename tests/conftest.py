20d9560b79fdf33bc595bda6667a9fdb59f8d6aa
Анна Краснокутская <anna.kharina.1998@mail.ru>
2025-06-24 10:45:01 +0300

    hw 11.8 comment attachements

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,9 +33,9 @@ def setup_browser(request):
 
     yield
 
-    tests.utils.attach.add_screenshot(browser)
-    tests.utils.attach.add_logs(browser)
-    tests.utils.attach.add_html(browser)
-    tests.utils.attach.add_video(browser)
+    # tests.utils.attach.add_screenshot(browser)
+    # tests.utils.attach.add_logs(browser)
+    # tests.utils.attach.add_html(browser)
+    # tests.utils.attach.add_video(browser)
 
     browser.quit()
