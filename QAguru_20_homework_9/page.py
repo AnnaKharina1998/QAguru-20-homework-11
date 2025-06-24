3c8b03bc46e4a0820f062c85bfe60132cdf3fdb3
Анна Краснокутская <anna.kharina.1998@mail.ru>
2025-06-24 10:07:03 +0300

    hw 11.5 delete ads

--- QAguru_20_homework_9/page.py
+++ QAguru_20_homework_9/page.py
@@ -6,6 +6,12 @@ from QAguru_20_homework_9.resourses import resource_path
 
 class RegistrationPage:
     def register_user(self, user: User):
+        browser.driver.execute_script("""
+            let ad = document.querySelector('.Google-Ad');
+            if (ad) { ad.remove(); }
+        """)
+        if browser.element('.ad-close-button').matching(be.visible):
+            browser.element('.ad-close-button').click()
         browser.open("https://demoqa.com/automation-practice-form")
         browser.element('#firstName').type(user.first_name)
         browser.element('#lastName').type(user.last_name)
